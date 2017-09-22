279082f42af001cc8603d7b9b5e6c23d9fc97ffe
zjwright <31255937+zjwright@users.noreply.github.com>
2017-09-21 21:39:46 -0500

    finished

--- dna2protein.m
+++ dna2protein.m
@@ -1,3 +1,13 @@
 function proteinseq = dna2protein(dnaseq,frame)
 % Input a dna sequence and a reading frame and returns the corresponding
-% protein sequence. 
+% protein sequence.
+if frame==1
+    proteinseq = nt2aa(dnaseq,'Frame',1);
+elseif frame==2
+    proteinseq = nt2aa(dnaseq,'Frame',2);
+elseif fram==3
+    proteinseq=nt2aa(dnaseq, 'Frame',3);
+else
+    disp('error: frame not 1, 2, or 3')
+end
+end
